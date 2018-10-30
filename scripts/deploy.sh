0e30556a47b5a830c7933060c3d04b3e7b5b9ad6
Jeff Parent <jeff.parent@yaengage.com>
2018-10-30 09:48:23 -0500

    Fixed deply name and errors

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,14 +5,19 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
+if [ "x${GITHUB_TOKEN}" == "x" ]; then
+  echo "GITHUB_TOKEN not set"
+  exit 1
+fi
+
 TAG="${1}"
 OS="${2}"
 
-echo "Deploying ${TAG} no ${OS}"
+echo "Deploying ${TAG} on ${OS}"
 github-release upload \
   --token "${GITHUB_TOKEN}" \
   --owner 'jecxjo' \
   --repo 'todo.hs' \
   --tag "${TAG}" \
   --file "$(stack path --local-install-root)/bin/todo" \
-  --name "todo-${OS}-${tag}"
+  --name "todo-${OS}-${TAG}"
